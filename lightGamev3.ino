d75ed9c510f174c30f3ea9146fae32f23c624830
Thomas Kneisel <thomaskneisel@users.noreply.github.com>
2019-12-12 20:26:33 +0100

    I like to code stuff
    fix indents, add changeColorTo function, clean some DRY code by reducing loops, IF > switch ;)

--- lightGamev3.ino
+++ lightGamev3.ino
@@ -60,115 +60,113 @@ void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
 
-currentPalette = CloudColors_p;
+    currentPalette = CloudColors_p;
     currentBlending = LINEARBLEND;
     
-pinMode(BUTTON_ONE_LED_PIN, OUTPUT);
-pinMode(BUTTON_TWO_LED_PIN, OUTPUT);
-digitalWrite(BUTTON_ONE_LED_PIN, HIGH);
-digitalWrite(BUTTON_TWO_LED_PIN, HIGH);
-  //setup LEDs
-   // tell FastLED there's XX NEOPIXEL leds on pin YY
-  FastLED.addLeds<WS2811, 2>(leds[0], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<WS2811, 3>(leds[1], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<WS2811, 4>(leds[2], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<WS2811, 5>(leds[3], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<WS2811, 6>(leds[4], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<WS2811, 7>(leds[5], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<WS2811, 8>(leds[6], NUM_LEDS_PER_STRIP);
+    pinMode(BUTTON_ONE_LED_PIN, OUTPUT);
+    pinMode(BUTTON_TWO_LED_PIN, OUTPUT);
+    digitalWrite(BUTTON_ONE_LED_PIN, HIGH);
+    digitalWrite(BUTTON_TWO_LED_PIN, HIGH);
+    //setup LEDs
+    // tell FastLED there's XX NEOPIXEL leds on pin YY
+    FastLED.addLeds<WS2811, 2>(leds[0], NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<WS2811, 3>(leds[1], NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<WS2811, 4>(leds[2], NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<WS2811, 5>(leds[3], NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<WS2811, 6>(leds[4], NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<WS2811, 7>(leds[5], NUM_LEDS_PER_STRIP);
+    FastLED.addLeds<WS2811, 8>(leds[6], NUM_LEDS_PER_STRIP);
 
- // Initialize the button1
-  button1.begin();
-  // Initialize the button2
-  button2.begin();
-  // Add the callback function to be called when the button1 is pressed.
-  button1.onPressed(onButton1Pressed);
-  // Add the callback function to be called when the button2 is pressed.
-  button2.onPressed(onButton2Pressed);
-  Serial.println("Ready......");
+    // Initialize the button1
+    button1.begin();
+    // Initialize the button2
+    button2.begin();
+    // Add the callback function to be called when the button1 is pressed.
+    button1.onPressed(onButton1Pressed);
+    // Add the callback function to be called when the button2 is pressed.
+    button2.onPressed(onButton2Pressed);
+    Serial.println("Ready......");
 }
 
 void loop() {
-  if(myTimer.isTimeReached()){ //check if execution time has been reached
-    tickTock();
-  }
+    if(myTimer.isTimeReached()) { //check if execution time has been reached
+        tickTock();
+    }
 }
 
 void onButton1Pressed() {
-  if(queueNewGame == false){
-    if(gameOn == true){
-      Serial.println("left");
-      if(playerX>0){
-        playerX--;
-      }
-   } else {
-    //start a new game!!!!
-      Serial.println("clicked, start a new one");
-      queueNewGame = true;  
+    if(queueNewGame == false){
+        if(gameOn == true){
+            Serial.println("left");
+            if(playerX>0){
+                playerX--;
+            }
+        } else {
+        //start a new game!!!!
+            Serial.println("clicked, start a new one");
+            queueNewGame = true;  
+        }
     }
-  }
 }
 
 void onButton2Pressed() {
-  if(queueNewGame == false){
-    if(gameOn == true){
-      Serial.println("right");
-        if(playerX<columns-1){
-          playerX++;
+    if(queueNewGame == false){
+        if(gameOn){
+            Serial.println("right");
+            if(playerX<columns-1) {
+                playerX++;
+            }
+        } else {
+            //start a new game!!!!
+            Serial.println("clicked, start a new one");
+            queueNewGame = true;  
         }
-    } else {
-      //start a new game!!!!
-        Serial.println("clicked, start a new one");
-        queueNewGame = true;  
-      }
-  }
+    }
 }
 
 void tickTock(){
-  button1.read();
-  button2.read();
-  if(queueNewGame == true){
-    startNewGame(); 
-  }
+    button1.read();
+    button2.read();
+    if(queueNewGame) {
+        startNewGame(); 
+    }
 
-  switch(gameOn){
-    case false:
-      //screensaver mode  
-      stepScreensaver();
-      break;
-    case true:
-      stepGame();
-      break;
-   }
-  renderGameBoard();
+    if(gameOn) {
+        stepGame();
+    } else {
+        //screensaver mode  
+        stepScreensaver();
+    }
+    renderGameBoard();
 }
-void startNewGame() {
-  Serial.println("HERE WE GO!");
-playerX = 3;
-gameLevel = 0;
-levelStepper = 30;  
-levelIncrease = 3; //how many clicks to condense next drop
-numToDrop = 3; //initial # to drop at once (random position, potential overlap)
-countUntilDrop = 0;
-waitForDrop = 0;
-waitForDropIncrease = 0;
-waitForDropThreshold = 5; 
-increaseDropThreshold = 35;
 
-  //clear gameboard
-  for (int i = columns-1; i >=0; i--) {
-        for (int j = rows-1; j >= 0; j--) {
-          gameArray[i][j] = 0;
+void startNewGame() {
+    Serial.println("HERE WE GO!");
+    playerX = 3;
+    gameLevel = 0;
+    levelStepper = 30;  
+    levelIncrease = 3; //how many clicks to condense next drop
+    numToDrop = 3; //initial # to drop at once (random position, potential overlap)
+    countUntilDrop = 0;
+    waitForDrop = 0;
+    waitForDropIncrease = 0;
+    waitForDropThreshold = 5; 
+    increaseDropThreshold = 35;
+
+    //clear gameboard
+    for (int column = columns-1; column >=0; column--) {
+        for (int row = rows-1; row >= 0; row--) {
+            gameArray[column][row] = 0;
         }
-   }
-  //reset all light colors;
-  // play startup sequence
-  gameOn = true;
-  queueNewGame = false;
+    }
+    //reset all light colors;
+    // play startup sequence
+    gameOn = true;
+    queueNewGame = false;
 }
 
 void stepGame(){
-  //Serial.println("stepGame");
+    //Serial.println("stepGame");
     countUntilDrop--;
 
     if(countUntilDrop <= 0){
@@ -247,116 +245,111 @@ void stepGame(){
 }
 
 void renderGameBoard() {
-  if(renderGameAsText == false){
-    //Serial.println("renderGameBoard");
-    FastLED.show();
-    delay(30);
-    
-  } else {
-    Serial.println("----------------------");
-      // This outer loop will go over each strip, one at a time
-    for (int i = columns-1; i >=0; i--) {
-        for (int j = rows-1; j >= 0; j--) {
-        int locValue = gameArray[i][j];
-        if(j==0 && i==playerX){
-           Serial.print("X");
-        } else {
-          if(locValue == 0){
-            Serial.print(" ");
-          } else{
-            Serial.print(locValue);
-          }
-        }
-      }
-      Serial.println("|");
-    }
-  }
-}
-void stepScreensaver() {
-  //Serial.println("stepScreensaver ");
-  //TODO: make this more interesting
-  static uint8_t colorIndex = 0;
-  screenSaverTimeOut+=1000;
- screenSaverToggle = !screenSaverToggle;
-  if(screenSaverTimeOut > 10000){
-     screenSaverTimeOut = 0;
-     uint16_t clr = CRGB::Black;
-      for(int i = 0; i < columns; i++) { 
-        // This inner loop will go over each led in the current strip, one at a time
-        for(int j = 0; j < rows; j++) {
-          switch(screenSaverSelector){
-            case 0:
-              if(screenSaverToggle){
-                leds[i][j] = CRGB::Red;
-              } else {
-                leds[i][j] = CRGB::Green;  
-              }
-            break;
-            case 1:
-            colorIndex +=3;
-              leds[i][j] = ColorFromPalette( currentPalette, colorIndex, 255, currentBlending);
-            break;
+    if(renderGameAsText == false) {
+        //Serial.println("renderGameBoard");
+        FastLED.show();
+        delay(30);
+    } else {
+        Serial.println("----------------------");
+        // This outer loop will go over each strip, one at a time
+        for (int i = columns-1; i >=0; i--) {
+            for (int j = rows-1; j >= 0; j--) {
+                int locValue = gameArray[i][j];
+                if(j==0 && i==playerX) {
+                    Serial.print("X");
+                } else {
+                    if(locValue == 0) {
+                        Serial.print(" ");
+                    } else {
+                    Serial.print(locValue);
+                    }
+                }
             }
-          
+            Serial.println("|");
+        }
+    }
+}
+
+void stepScreensaver() {
+    //Serial.println("stepScreensaver ");
+    //TODO: make this more interesting
+    static uint8_t colorIndex = 0;
+    screenSaverTimeOut+=1000;
+    screenSaverToggle = !screenSaverToggle;
+    if(screenSaverTimeOut > 10000) {
+        screenSaverTimeOut = 0;
+        uint16_t clr = CRGB::Black;
+        if(screenSaverSelector==0) {
+            if(screenSaverToggle){
+                changeColorTo(CRGB::Red);
+            } else {
+                changeColorTo(CRGB::Green);
+            }
+        } else {
+            for(int column = 0; column < columns; column++) { 
+                // This inner loop will go over each led in the current strip, one at a time
+                for(int j = 0; j < rows; j++) {
+                    colorIndex +=3;
+                    leds[i][j] = ColorFromPalette(currentPalette, colorIndex, 255, currentBlending);
+                }
+            }
         }
         delay(30);
-      }  
-  }
+    }
 }
 
 void checkCollision(int k) {
     if(playerX == k){
-      //Collision found
-      gameOn = false;
-      playLoseAnim();
+        //Collision found
+        gameOn = false;
+        playLoseAnim();
     }
-  }
+}
 
 void dropPiece() {
-  // set random top row location
-  countUntilDrop = levelStepper;
-  waitForDrop++;
-  if(waitForDrop==waitForDropThreshold && levelStepper>=0){
-    waitForDrop = 0;
-    levelStepper-=levelIncrease; 
-    gameLevel++; 
-  }
-  waitForDropIncrease++;
-  if(waitForDropIncrease == increaseDropThreshold){
-    waitForDropIncrease = 0;
-    numToDrop++; 
-  }
-  if( numToDrop >NUM_STRIPS-2){
-    numToDrop = NUM_STRIPS-2; //Always leave at least 2 positions open ofr the player
-  }
-  for( int z=0;z<numToDrop;z++){
-    int ranX = random(0, columns);
-    gameArray[ranX][0] = 1;
-  }
+    // set random top row location
+    countUntilDrop = levelStepper;
+    waitForDrop++;
+    if(waitForDrop==waitForDropThreshold && levelStepper>=0) {
+        waitForDrop = 0;
+        levelStepper-=levelIncrease; 
+        gameLevel++; 
+    }
+    waitForDropIncrease++;
+    if(waitForDropIncrease == increaseDropThreshold) {
+        waitForDropIncrease = 0;
+        numToDrop++; 
+    }
+    if(numToDrop >NUM_STRIPS-2) {
+        numToDrop = NUM_STRIPS-2; //Always leave at least 2 positions open ofr the player
+    }
+    for(int z=0;z<numToDrop;z++) {
+        int ranX = random(0, columns);
+        gameArray[ranX][0] = 1;
+    }
 }
 
 void playLoseAnim() {
-  
-  for(int k = 0; k < 3; k++) {
-    Serial.println("YOU LOSE!");
-    for(int i = 0; i < columns; i++) {
-      for(int j = 0; j < rows; j++) {
-        leds[i][j] = CRGB::Red; 
-      }
+    for(int k = 0; k < 3; k++) {
+        Serial.println("YOU LOSE!");
+        changeColorTo(CRGB::Red);
+        FastLED.show();
+        delay(250);
+        changeColorTo(CRGB::Black);    
+        FastLED.show();
+        delay(250);
+        //switch scrrensaver
+        screenSaverSelector++;
+        if (screenSaverSelector > 1){
+            screenSaverSelector = 0;  
+        }
     }
-    FastLED.show();
-    delay(250);
-    for(int i = 0; i < columns; i++) {
-      for(int j = 0; j < rows; j++) {
-        leds[i][j] = CRGB::Black;
-      }
+}
+
+void changeColorTo(CRGB color) {
+    for(int column = 0; column < columns; column++) {
+        for(int row = 0; row < rows; row++) {
+            leds[column][row] = color; 
+        }
     }
-    FastLED.show();
-    delay(250);
-    //switch scrrensaver
-    screenSaverSelector++;
-    if (screenSaverSelector > 1){
-      screenSaverSelector = 0;  
-    }
-  }
 }
